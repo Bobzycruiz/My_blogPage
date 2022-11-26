e7ea9cca26e8668bc278cf07dc46c908c0f77f4e
Bobzycruiz <112961814+Bobzycruiz@users.noreply.github.com>
2022-11-26 08:57:44 +0100

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        @font-face {
+    font-family: 'futuraMediumBT';
+    src: url('Fonts/futuraMediumBT.eot');
+    src: local('futuraMediumBT'), url('Fonts/futuraMediumBT.woff') format('woff'), url('Fonts/futuraMediumBT.ttf') format('truetype');
+}
+.fontsforweb_fontid_61656 {
+    font-family: 'futuraMediumBT' !important;
+}
+</style>
+<link rel="stylesheet" href="../css/feed.css">
+<link
+rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,500;0,600;1,300;1,400&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <section></section>
+    <h2>Bobzy's blog <i class="fas fa-blog"></i></h2>
+    <div class="con">
+ <div class="container">
+    <form>
+        <h1>
+            Give Your Feedback
+        </h1>
+        <div class="id">
+            <input type="text" placeholder="Full Name" required>
+            <i class="fas fa-user"></i>
+        </div>
+        <div class="id">
+            <input type="Email" placeholder="Email Address" required>
+            <i class="fas fa-envelope"></i>
+        </div>
+        <textarea cols="15" rows="5" placeholder="Enter Your Opinions here..." required></textarea>
+        <button>Send</button>
+    </form>
+ </div>
+ 
+</div>
+
+ <div class="left_footer">
+    <p>2022 Copyright&#169 by Bobzycruiz AB <br>🇳🇬️ Nigeria</p>
+</div>
+</body>
+</html>
